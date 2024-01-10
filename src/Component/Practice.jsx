4099374ebd5c9fa7aa1d639e9568b7472745f831
sonu <sonupandeybaldeo2003@gmail.com>
2024-01-10 23:49:48 +0530

    10/01/2024

---
+++ src/Component/Practice.jsx
@@ -0,0 +1,70 @@
+
+//! notebook Questions ---------->>>>>>>
+
+//? 1 what is react & why is so popular  ?
+//? 2 what is features of react js ?
+//? 4 what is jsx ?
+//? 5 what is react fragment ?
+//? 6 what are the hight order component and its use with example ?
+//? 7 what is life-cycle method & its methods ?
+//? 8 what is component & types of component & what you preffred ?
+//? 9 what is prop-dreling in react ?
+//? 10 what is key in react & use of key & why use key ?
+//? 11 can jsx directly read by the web browser ?
+//? 12 why use react instends of other framworks ?
+//? 13 
+//? 14 
+//? 15 
+//? 16 
+//? 17 
+//? 18 
+//? 19 
+//? 20 
+//? 21 
+//? 22 
+//? 23 
+//? 24 
+//? 25 
+//? 26 
+//? 27 
+//? 28 
+//? 29 
+//? 30 
+//? 31 
+//? 32 
+//? 33 
+//? 34 
+//? 35 
+//? 36 
+//? 37 
+//? 38 
+//? 39 
+//? 40 
+//? 41 
+//? 42 
+//? 43 
+//? 44 
+//? 45 
+//? 46 
+//? 47 
+//? 48 
+//? 49 
+//? 50 
+//? 51 
+//? 52 
+//? 53 
+//? 54 
+//? 55 
+//? 56 
+//? 57 
+//? 58 
+//? 59 
+//? 60 
+//? 61 
+//? 62 
+//? 63 
+//? 64 
+//? 65 
+//? 66 
+//? 67 
+//? 68 
