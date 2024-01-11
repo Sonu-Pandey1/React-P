8c703b88eea59e65f030d061ed458d66c6107e41
sonu <sonupandeybaldeo2003@gmail.com>
2024-01-11 21:12:33 +0530

    11/01/2024

--- src/Component/Practice.jsx
+++ src/Component/Practice.jsx
@@ -12,59 +12,60 @@
 //? 10 what is key in react & use of key & why use key ?
 //? 11 can jsx directly read by the web browser ?
 //? 12 why use react instends of other framworks ?
-//? 13 
-//? 14 
-//? 15 
-//? 16 
-//? 17 
-//? 18 
-//? 19 
-//? 20 
-//? 21 
-//? 22 
-//? 23 
-//? 24 
-//? 25 
-//? 26 
-//? 27 
-//? 28 
-//? 29 
-//? 30 
-//? 31 
-//? 32 
-//? 33 
-//? 34 
-//? 35 
-//? 36 
-//? 37 
-//? 38 
-//? 39 
-//? 40 
-//? 41 
-//? 42 
-//? 43 
-//? 44 
-//? 45 
-//? 46 
-//? 47 
-//? 48 
-//? 49 
-//? 50 
-//? 51 
-//? 52 
-//? 53 
-//? 54 
-//? 55 
-//? 56 
-//? 57 
-//? 58 
-//? 59 
-//? 60 
-//? 61 
-//? 62 
-//? 63 
-//? 64 
-//? 65 
-//? 66 
-//? 67 
-//? 68 
+//? 13 how react diffrent with angular ?
+//? 14 what is react-router ? is`s work ?
+//? 15 what is state ?
+//? 16 what is props ?
+//? 17 diffrence bitween state and props ?
+//? 18 what is controled component ?
+//? 19 what is uncontroled component ?
+//? 20 what is statefull component ?
+//? 21 what is stateless component ?
+//? 22 what is pure compponent diffrence bitween pure and high-order-component ?
+//? 23 what is lazy-loading ?
+//? 24 what is error-boundery ?
+//? 25 what is life-cycle method and its`s method ?
+//? 26 what is hooks in react & it`s type explain all with exapmple ?
+//? 27 what is render ?
+//? 28 what is dom , diffrence bitween virtual dom and real dom ?
+//? 29 what is reconsiliation ? 
+//? 30 what are the technologyes used to optimize react app ?
+//? 31 what is event and synthic event in react ?
+//? 32 what is unideractional flow ?
+//? 33 what is flux ?
+//? 34 what is the diffrence bitween redux and flux ?
+//? 35 what is redux ?
+//? 36 diffrence bitween react js and redux ?
+//? 37 diffrence bitween redux and context api/use context ?
+//? 38 what is middleware ?
+//? 39 what is redux and what is redux flow ? explain it ?
+
+
+
+//todo--->>>  pc/ Logicial Questions ----->>>>>
+
+
+//! 1 example of Function based component, use of use-state props, props in destrustred way and also as pass a spread oprator way obj ?
+
+//! 2 example of classs based componenrt  props,stste,defalut props ,propstype ?
+//! 3 hide and show h1 on click ?
+//! 4 toggle h1 on click ? 
+//! 5 get the data from form ?
+//! 6 basic form validition ?
+//! 7 explain life cycle methods and is method ?
+//! 8 explain hook and there types with example ?
+//! 9 lifting state / send data  child components to parent component ?
+//! 10 explain high order component with example ?
+//! 11 get previous state in function based component with example ?
+//! 12 previous props in function based component with example ?
+//! 13 state with object means that state main object pas karne par issue is that whole object are chnage when we change only one property so avoide this we use spard oprator ?
+
+//! 14 setup routing togather nexted routing and dyanamic routing ?
+//! 15 use of seacrchParams and setsearchParamas hooks in rrd // for give the output on the basics of what will serch/query in the url ?
+
+//! 16 navigating on click / use of usenavigate hooks in rrd ?
+//! 17 use of  use location hooks/ gives all the data of parent component  / reeadony porpuse i dont gt
+//! 18 use protected route hooks  ( we also do that with the help of high-order component where we can paas a condition that if user is active then navigate to children(routes) else navigate login) ?
+
+
+
